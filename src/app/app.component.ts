e0ea086ea458379e6db613d6a6bf0eecd34f958d
joako-18 <joako1327@gmai.com>
2024-09-25 00:27:40 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./component/header/header.component";
+import { FooterComponent } from "./component/footer/footer.component";
+import { DashboardComponent } from "./component/dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/component/dashboard/dashboard.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.scss',
+  encapsulation: ViewEncapsulation.None
+})
+export class DashboardComponent {
+
+}
